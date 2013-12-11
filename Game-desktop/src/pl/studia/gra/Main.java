57bd156250a3aa38f50152312d8a5fca058dc89e
klamkiewicz <icespam@wp.pl>
2013-12-11 10:57:03 +0100

    Basics

---
+++ Game-desktop/src/pl/studia/gra/Main.java
@@ -0,0 +1,16 @@
+package pl.studia.gra;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Game";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new GameClass(), cfg);
+	}
+}
