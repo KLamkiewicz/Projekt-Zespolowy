e0705794f98bc6238a958909af8b20214ea14638
Jarosław <cimochjaroslaw@bdimension.pl>
2014-01-12 17:24:31 +0100

    Handling Different Resolutions 1.0

--- Game/src/pl/studia/gra/GameClass.java
+++ Game/src/pl/studia/gra/GameClass.java
@@ -1,38 +1,35 @@
 package pl.studia.gra;
 
+import pl.studia.util.Constants;
+import pl.studia.util.ResolutionManager;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
-public class GameClass implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	public class GameClass implements ApplicationListener {
+	private SpriteBatch 		batch;
+	private Texture 			texture;
+	private Sprite 				sprite;
+	private Rectangle 			viewport;
+	private OrthographicCamera 	cam;
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+		texture = new Texture(Gdx.files.internal("data/kk.jpg"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear); // ustawienie Filtrowania tex, dla skalowania w dol i gore ( mozna ustawiæ rozne filtry )
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		sprite = new Sprite(texture);
+		cam = new OrthographicCamera(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT);	
+		cam.position.set(Constants.VIRTUAL_WIDTH/2,Constants.VIRTUAL_HEIGHT/2, 0); // 3 parametr nie istotny przy 2d
+	
 	}
 
 	@Override
@@ -43,10 +40,27 @@ public class GameClass implements ApplicationListener {
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		GL20 gl = Gdx.graphics.getGL20(); // OpenGL 2.0 umo¿liwia ³adowanie dowolnego rozmiaru textur TYMCZASOWO
 		
-		batch.setProjectionMatrix(camera.combined);
+		// update camera
+        cam.update();
+        //cam.apply(Gdx.gl10); only in GL1.0
+       
+        // set viewport
+        Gdx.gl.glViewport((int) viewport.x, (int) viewport.y,
+                          (int) viewport.width, (int) viewport.height);
+ 
+        // clear previous frame
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        
+		// Texturing --------------------- /
+		gl.glActiveTexture(GL10.GL_TEXTURE0);
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		
+		//texture.bind();
+		
+		batch.setProjectionMatrix(cam.combined);
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
@@ -54,6 +68,16 @@ public class GameClass implements ApplicationListener {
 
 	@Override
 	public void resize(int width, int height) {
+	
+	
+		 ResolutionManager.scaleDisplayResolution(width,height);
+		 
+		 float w = (float)Constants.VIRTUAL_WIDTH*ResolutionManager.scale;
+	     float h = (float)Constants.VIRTUAL_HEIGHT*ResolutionManager.scale;
+	     viewport = new Rectangle(ResolutionManager.crop.x, ResolutionManager.crop.y, w, h); // pocz¹tkowo 0,0 
+	     System.out.println(ResolutionManager.crop.x+" "+ResolutionManager.crop.y);
+	
+	
 	}
 
 	@Override
---
+++ Game/src/pl/studia/util/Constants.java
@@ -0,0 +1,12 @@
+package pl.studia.util;
+
+public class Constants {
+
+	/////////////////////////////////// Sta³e Ekran ///////////////////////////////////
+	public static final int VIRTUAL_WIDTH = 1920;
+	public static final int VIRTUAL_HEIGHT = 1080;
+	public static final float ASPECT_RATIO = (float)VIRTUAL_WIDTH/(float)VIRTUAL_HEIGHT;
+	
+	
+	
+}
