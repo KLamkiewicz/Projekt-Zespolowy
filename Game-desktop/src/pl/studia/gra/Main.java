578b15da47e6e994223d1fdc9dca4341b5c7f0f7
Jarosław <cimochjaroslaw@bdimension.pl>
2014-01-12 17:50:59 +0100

    Handling Different Resolutions 1.1

--- Game-desktop/src/pl/studia/gra/Main.java
+++ Game-desktop/src/pl/studia/gra/Main.java
@@ -1,15 +1,21 @@
 package pl.studia.gra;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		
+		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		cfg.title = "Game";
 		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 600;
+		//cfg.fullscreen=true;
+		cfg.width = screenDimension.width;
+		cfg.height= screenDimension.height;
+	
 		
 		new LwjglApplication(new GameClass(), cfg);
 	}
