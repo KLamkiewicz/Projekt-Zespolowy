b103676e5885031fdc3c3783c122811d1e0ea76c
klamkiewicz <icewind91@wp.pl>
2014-04-09 19:26:40 +0200

    Structure for the object movement

--- Game/src/pl/studia/objects/GameObject.java
+++ Game/src/pl/studia/objects/GameObject.java
@@ -1,19 +1,77 @@
 package pl.studia.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class GameObject {
 	public Vector2 position;
 	public Vector2 dimension;
 	public Vector2 origin;
+	public Vector2 velocity;			//Object current speed
+	public Vector2 terminalVelocity;	//Object maximum speed
+	public Vector2 friction;			//Object friction
+	public Vector2 acceleration;		//Object acceleration
+	public Rectangle bounds;			//Object area used to determine collision
+	
 
 	public GameObject(){
 		position = new Vector2();
-		dimension = new Vector2();
+		dimension = new Vector2(1, 1);
 		origin = new Vector2();
+		velocity = new Vector2();
+		terminalVelocity = new Vector2(1, 1);
+		//friction = new Vector2();
+		acceleration = new Vector2();
+		bounds = new Rectangle();
 	}
 	
-	public void update (float deltaTime) {}
+	public void update (float deltaTime) {
+		updateMotionX(deltaTime);
+		updateMotionY(deltaTime);
+		// Move the object to the new position
+		position.x += velocity.x * deltaTime;
+		position.y += velocity.y * deltaTime;
+	}
 	public abstract void render (SpriteBatch batch);
+	
+	/**Check for both negative and positive velocity, if it's not equal to 0 object is moving
+	 * Apply friction
+	 * If velocity is positive, apply friction max out of (velocity - friction, 0)
+	 * 0 so the velocity doesn't change its sign
+	 * Next acceleration is applied to the velocity
+	 * Finally use the clamp function (value, min, max) so the velocity doesn't go beyond the limits
+	 */
+	protected void updateMotionX (float deltaTime) {
+		if (velocity.x != 0) {
+			if (velocity.x > 0) {
+				velocity.x = Math.max(velocity.x - friction.x * deltaTime, 0);
+			} else {
+				velocity.x = Math.min(velocity.x + friction.x * deltaTime, 0);
+			}
+		}
+		// Apply acceleration
+		velocity.x += acceleration.x * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.x = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
+	}
+	
+	protected void updateMotionY (float deltaTime) {
+		if (velocity.y != 0) {
+			// Apply friction
+			if (velocity.y > 0) {
+				velocity.y = Math.max(velocity.y - friction.y * deltaTime, 0);
+			} else {
+				velocity.y = Math.min(velocity.y + friction.y * deltaTime, 0);
+			}
+		}
+		// Apply acceleration
+		velocity.y += acceleration.y * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.y = MathUtils.clamp(velocity.y, -terminalVelocity.y, terminalVelocity.y);
+	}
+	
 }
